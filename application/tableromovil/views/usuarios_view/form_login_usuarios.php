cd6009f386764e4ec2c9b92523599347fdb6dbb1
Juan <jpmayoral@gmail.com>
2012-03-30 11:03:31 -0300

    Agrego interfaz de seguridad, y refrigeracion

--- application/tableromovil/views/usuarios_view/form_login_usuarios.php
+++ application/tableromovil/views/usuarios_view/form_login_usuarios.php
@@ -1,11 +1,11 @@
 <form action="<?=base_url()?>welcome/login" method="post" name="formLoginusuarios" id="formLoginusuarios">
 	<div data-role="fieldcontain">
 		<label for="usuarios_username">Usuario:</label>
-		<input type="text" name="usuarios_username" id="usuarios_username" value="" />
+		<input type="text" name="usuarios_username" id="usuarios_username" value="" data-theme="b"/>
 	</div>
 	<div data-role="fieldcontain">
 		<label for="usuarios_password">Contraseña:</label>
-		<input type="password" name="usuarios_password" id="usuarios_password" />
+		<input type="password" name="usuarios_password" id="usuarios_password" data-theme="b"/>
 	</div>
 	<input type="submit" name="Aceptar"  value="Aceptar" data-role="button" data-theme="b"/>
 	<div class="errors" id="errors">
