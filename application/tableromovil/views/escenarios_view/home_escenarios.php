96ab5620abe69d115fb66b13f5e2c0f6f7317a52
Juan <jpmayoral@gmail.com>
2012-05-01 10:30:03 -0300

    agrego icono para escenarios

--- application/tableromovil/views/escenarios_view/home_escenarios.php
+++ application/tableromovil/views/escenarios_view/home_escenarios.php
@@ -1,9 +1,6 @@
 <ul data-role="listview" data-split-icon="gear" data-theme="a">
-<?php foreach($query as $q):?>
-		
+	<?php foreach($query as $q):?>
 		<li><a href="#"><img src="<?php echo base_url("css/images/".$q->escenarios_iconpath)?>"><?php echo $q->escenarios_descripcion;?></a><a href="#"></a></li>
-	
-			
 	<?php endforeach; ?>
 </ul>
 
