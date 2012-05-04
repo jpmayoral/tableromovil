b2cc81337d59824c19bf19105c5c5f35c59ed219
Johnny <johnny@johnny-desktop.(none)>
2012-05-04 03:14:30 -0300

    Se dio funcionalidad al módulo de iluminacion y se añadió la parte de configuración de lasa salidas digitales en la sección de administración, ademas se creó un usuario super admin que tiene acceso a la parte de configuración de las salidas digitales

--- application/tableromovil/views/iluconfig_view/home_iluconfig.php
+++
@@ -1,8 +0,0 @@
-<?=$this->load->view('default/_header')?>
-<?php if($this->session->flashdata('flashConfirm')) echo $this->session->flashdata('flashConfirm'); ?>
-<?php if($this->session->flashdata('flashError')) echo $this->session->flashdata('flashError');?>
-
-<?php if($flag['i']):?>
-	<a href="<?=base_url()?>index.php/iluconfig_controller/add_c" title='Nuevo'>Nuevo</a>
-<?php endif; ?>
-
