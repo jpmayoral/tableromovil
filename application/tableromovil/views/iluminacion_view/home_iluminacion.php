35eaf5ed9650a63654396f7f05a89a28f5f85ec8
Johnny <guzmanjhonny@gmail.com>
2012-08-17 06:26:51 -0300

    cambios

--- application/tableromovil/views/iluminacion_view/home_iluminacion.php
+++
@@ -1,8 +0,0 @@
-<div class="ui-body ui-body-j">	
-		<div id="content_iluminacion">
-			<?=$this->load->view("iluminacion_view/record_list_iluminacion")?>
-		</div>
-</div>
-<script type="text/javascript"> 
-	var t = setInterval("updateContent('<?=base_url()?>iluminacion_controller/search_c/','content_iluminacion')",1000);
-</script>
